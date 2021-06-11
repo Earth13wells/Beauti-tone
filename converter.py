bda0305be3c5aae645c00a2c7a96f370f22cf3d9
earth13wells <Gabrielw.presley@protonmail.com>
2021-06-11 00:01:13 -0700

    Fixed linting issues

--- converter.py
+++ converter.py
@@ -10,21 +10,22 @@
 
 try:
     import Tkinter as tk
-    from Tkinter import ttk
 except ModuleNotFoundError:
     import tkinter as tk
-    from tkinter import ttk
 
+# Defining the main window
 first = tk.Tk()
-# old lables titles
+
+# Old lables headers
 tk.Label(first, text="Ounces:").grid(row=0, column=0)
 tk.Label(first, text="Shots:").grid(row=0, column=1)
 tk.Label(first, text="1/2 Shots:").grid(row=0, column=2)
 tk.Label(first, text="1/4 Shots:").grid(row=0, column=3)
 
-#Conversion function
+
+# Conversion function
 def convert():
-    for z in range(0,7):
+    for z in range(0, 7):
         New_Ounces[z].config(text=Old_Ounces[z].get())
         New_Shots[z].config(text=Old_Shots[z].get())
         if(Old_Quarters[z].get() == ""):
@@ -33,25 +34,30 @@ def convert():
             Old_Halfs[z].insert(0, "0")
         try:
             Sixteenths = (int(Old_Quarters[z].get())*4 + int(Old_Halfs[z].get())*8)
+            if(Sixteenths >= 17):
+                tk.Label(first, text="TOO MANU FRACTIONAL SHOTS").grid(row=8, column=4)
             New_Sixteenths[z].config(text=Sixteenths)
         except ValueError:
-            tk.Label(first, text=f"INVALID INPUT").grid(row=8, column=4)
+            tk.Label(first, text="INVALID INPUT").grid(row=8, column=4)
+
 
 # clear function
 def clear():
-    for p in range(0,7):
+    for p in range(0, 7):
         Old_Ounces[p].delete(0, 100)
         Old_Shots[p].delete(0, 100)
         Old_Halfs[p].delete(0, 100)
         Old_Quarters[p].delete(0, 100)
-#Defining some empty arrays to fill with layout
-Old_Ounces   = [[],[],[],[],[],[],[]]
-Old_Shots    = [[],[],[],[],[],[],[]]
-Old_Halfs    = [[],[],[],[],[],[],[]]
-Old_Quarters = [[],[],[],[],[],[],[]]
 
-#filling arrays with layout
-for i in range(0,7):
+
+#  Defining some empty arrays to fill with layout
+Old_Ounces = [[]]*7
+Old_Shots = [[]]*7
+Old_Halfs = [[]]*7
+Old_Quarters = [[]]*7
+
+# Filling arrays with layout
+for i in range(0, 7):
     Old_Ounces[i] = tk.Entry(first)
     Old_Shots[i] = tk.Entry(first)
     Old_Halfs[i] = tk.Entry(first)
@@ -62,12 +68,12 @@ for i in range(0,7):
     Old_Halfs[i].grid(row=i+1, column=2)
     Old_Quarters[i].grid(row=i+1, column=3)
 
-#convert_button
-convert_button = tk.Button( first, text="Convert", command = convert) # Todo add function
+#  Convert_button
+convert_button = tk.Button(first, text="Convert", command=convert)
 convert_button.grid(row=i+1, column=4)
 
-#clear_button
-clear_button = tk.Button( first, text="Clear", command = clear) # Todo add function
+#  clear_button
+clear_button = tk.Button(first, text="Clear", command=clear)
 clear_button.grid(row=i, column=4)
 
 # new lables titles
@@ -75,15 +81,15 @@ tk.Label(first, text="Ounces:").grid(row=0, column=5)
 tk.Label(first, text="Shots:").grid(row=0, column=6)
 tk.Label(first, text="1/16 Shots:").grid(row=0, column=7)
 
-#Defining some empty arrays to fill with layout
-New_Ounces     = [[],[],[],[],[],[],[]]
-New_Shots      = [[],[],[],[],[],[],[]]
-New_Sixteenths = [[],[],[],[],[],[],[]]
+#  Defining some empty arrays to fill with layout
+New_Ounces = [[]]*7
+New_Shots = [[]]*7
+New_Sixteenths = [[]]*7
 
-#filling arrays with layout
+#  Filling arrays with layout
 first.grid_columnconfigure(1, weight=1)
 
-for i in range(0,7):
+for i in range(0, 7):
     New_Ounces[i] = tk.Entry(first)
     New_Shots[i] = tk.Entry(first)
     New_Sixteenths[i] = tk.Entry(first)
